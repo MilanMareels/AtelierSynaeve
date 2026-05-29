1087710e84b56a4d504d0d62fa8482fb1d7994dd
MilanMareels <milan.mareels@gmail.com>
2026-05-29 17:45:05 +0200

    testing portainer changes pull

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,20 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-[#f4f5f7] text-white font-sans relative overflow-hidden">
       <div className="absolute inset-0 z-0 pointer-events-none">
-        <Image src="/Logo/logo.webp" alt="Atelier Synaeve Aquarel Achtergrond" fill className="object-cover scale-150 md:scale-200 transform transition-transform duration-500" priority />
+        <Image
+          src="/Logo/logo.webp"
+          alt="Atelier Synaeve Aquarel Achtergrond"
+          fill
+          className="object-cover scale-150 md:scale-200 transform transition-transform duration-500"
+          priority
+        />
       </div>
 
       <main className="grow flex flex-col justify-center items-center text-center z-10 px-4 md:px-6 relative w-full">
         <div className="flex flex-col w-max max-w-full">
-          <h1 className="text-[7.5vw] sm:text-4xl md:text-5xl lg:text-[4.5rem] uppercase mb-2 drop-shadow-md syncopate-bold tracking-tight whitespace-nowrap">Atelier Synaeve</h1>
+          <h1 className="text-[7.5vw] sm:text-4xl md:text-5xl lg:text-[4.5rem] uppercase mb-2 drop-shadow-md syncopate-bold tracking-tight whitespace-nowrap">
+            Atelier Synaeve Hallo
+          </h1>
 
           <div className="flex flex-row justify-between w-full px-1">
             <Link
